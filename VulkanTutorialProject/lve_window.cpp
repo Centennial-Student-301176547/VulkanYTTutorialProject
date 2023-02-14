8b77b93ffb15113074ca767b8a0be377a007443b
Charlie Bi <charlie_bi@hotmail.com>
2023-02-14 16:15:02 -0500

    Tutorial 01 Completed

---
+++ VulkanTutorialProject/lve_window.cpp
@@ -0,0 +1,28 @@
+#include "lve_window.hpp"
+
+namespace lve {
+
+
+	LveWindow::LveWindow(int w, int h, std::string name) : width{ w }, height{ h }, windowName{ name } {
+		initWindow();
+	}
+
+	LveWindow::~LveWindow() {
+		glfwDestroyWindow(windowPtr);
+		glfwTerminate();
+	}
+
+	void LveWindow::initWindow() {
+		glfwInit(); //initalizes glfw library
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); //tells glfw not to create an OpenGL context (we're using vulkan)
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); //disables ability to resize the window after init, resize will be handled using a special function
+
+		windowPtr = glfwCreateWindow(width, height, windowName.c_str(), nullptr, nullptr);
+	}
+
+	bool LveWindow::shouldClose() {
+
+		return glfwWindowShouldClose(windowPtr);
+	}
+
+}
---
+++ VulkanTutorialProject/lve_window.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN //include vulkan headers along with glfw
+#include <GLFW/glfw3.h>
+
+//std
+#include <string>
+
+namespace lve {
+
+	class LveWindow {
+
+	public:
+		LveWindow(int w, int h, std::string name);
+		~LveWindow();
+
+		//remove copy constructor to avoid dangling pointer
+		LveWindow(const LveWindow&) = delete;
+		LveWindow& operator=(const LveWindow&) = delete;
+
+		bool shouldClose();
+
+	private:
+
+		void initWindow();
+
+		const int width;
+		const int height;
+
+		std::string windowName;
+		GLFWwindow* windowPtr;
+	};
+
+
+}
