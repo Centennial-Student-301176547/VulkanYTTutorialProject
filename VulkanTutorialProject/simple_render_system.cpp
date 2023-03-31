eaa95583b1fa5592a68fe343b150ad0a1964e507
Charlie Bi <charlie_bi@hotmail.com>
2023-03-30 22:24:28 -0400

    Tutorial 19 Completed

--- VulkanTutorialProject/simple_render_system.cpp
+++ VulkanTutorialProject/simple_render_system.cpp
@@ -61,10 +61,11 @@ namespace lve {
     }
 
     void SimpleRenderSystem::renderGameObjects(
-        VkCommandBuffer commandBuffer, std::vector<LveGameObject>& gameObjects, const LveCamera& camera) {
-        lvePipelinePtr->bind(commandBuffer);
+        FrameInfo& frameInfo, std::vector<LveGameObject>& gameObjects) {
 
-        auto projectionView = camera.getProjection() * camera.getView();
+        lvePipelinePtr->bind(frameInfo.commandBuffer);
+
+        auto projectionView = frameInfo.camera.getProjection() * frameInfo.camera.getView();
 
         for (auto& obj : gameObjects) {
             SimplePushConstantData push{};
@@ -73,14 +74,14 @@ namespace lve {
             push.normalMatrix = obj.transform.normalMatrix();
 
             vkCmdPushConstants(
-                commandBuffer,
+                frameInfo.commandBuffer,
                 pipelineLayout,
                 VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
                 0,
                 sizeof(SimplePushConstantData),
                 &push);
-            obj.model->bind(commandBuffer);
-            obj.model->draw(commandBuffer);
+            obj.model->bind(frameInfo.commandBuffer);
+            obj.model->draw(frameInfo.commandBuffer);
         }
     }
 
